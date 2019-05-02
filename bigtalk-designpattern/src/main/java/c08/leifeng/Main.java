4c744e459073646c397ed202b32176ad3dda0ed6
leihenqingze <leihenqingze@sina.com>
2019-05-02 18:12:01 +0800

    大话设计模式-工厂方法模式

--- bigtalk-designpattern/src/main/java/c08/leifeng/Main.java
+++ bigtalk-designpattern/src/main/java/c08/leifeng/Main.java
@@ -7,6 +7,7 @@ public class Main {
     @Test
     public void simpleFactory() {
         //三句重复的代码，有重复也就有了坏味道
+        //有简单工厂是一个静态方法，所以每次创建对象都需要指定创建的类型，就造成了重复
         LeiFeng studentA = new SimpleFactory().createLeiFeng("学雷锋的大学生");
         studentA.buyRice();
         LeiFeng studentB = new SimpleFactory().createLeiFeng("学雷锋的大学生");
@@ -18,6 +19,7 @@ public class Main {
     @Test
     public void Factory() {
         //要换成'社区志愿者'，修改这里就可以
+        //有每个工厂只创建同类型的产品，只要更改对应的工厂，则其创建的对象类型都会被更改
         Factory factory = new UndergraduateFactory();
         LeiFeng studentA = factory.createLeiFeng();
         studentA.buyRice();
