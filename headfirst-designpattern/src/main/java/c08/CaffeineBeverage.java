49d7f385c9268f37888be4921803052520ffc5ba
leihenqingze <leihenqingze@sina.com>
2019-05-19 19:11:26 +0800

    Head First设计模式-模板方法模式

---
+++ headfirst-designpattern/src/main/java/c08/CaffeineBeverage.java
@@ -0,0 +1,27 @@
+package c08;
+
+public abstract class CaffeineBeverage {
+
+    /*
+    final方法，不希望子类覆盖它
+     */
+    final void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
+
+    abstract void brew();
+
+    abstract void addCondiments();
+
+    void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+
+}
